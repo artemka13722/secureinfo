b5c3f7b7e3180cbecea98da2c3193a6399ce9df6
art.skorkin <artemka.skorkin@gmail.com>
2020-11-03 15:16:20 +0700

    lab 3 fix

--- src/main/java/lab3/RSA.java
+++ src/main/java/lab3/RSA.java
@@ -1,48 +1,60 @@
 package lab3;
 
 import lab1.Lab1;
+import org.apache.commons.codec.digest.DigestUtils;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.security.SignatureException;
 
 public class RSA extends Lab1 {
 
-    public void test() throws IOException {
-        long p = getLongPrime(4);
-        long q = getLongPrime(4);
+    public void test(String inputFile) throws IOException, SignatureException {
 
-        long n = p * q;
-        long f = (p - 1) * (q - 1);
+        long p,q,N;
+        do {
+            p = getLongPrime(4);
+            q = getLongPrime(4);
+            N = p * q;
+        } while (128 > N);
 
-        long d = genLongLimit(f - 1);
-        while (gcd(d, f)[0] != 1) {
+        long fn = (p - 1) * (q - 1);
+
+        long d = genLongLimit(fn - 1);
+        while (gcd(d, fn)[0] != 1){
             d -= 1;
         }
 
-        long c = gcd(f, d)[2];
-        if (c < 0) {
-            c += f;
+        long c = gcd(fn, d)[2];
+        if(c < 0){
+            c += fn;
         }
 
-        long h = hashy("test.txt");
-
-        long s = powMod(h, c, n) % f;
-
-        long w = powMod(s,d,n) % f;
-
-        if(w == h){
-            System.out.println("Signature is correct");
-        }
-
+        long s = signFileMD5(inputFile, c, N);
+        checkSign(inputFile, s, d, N);
     }
 
-    public long hashy(String filename) throws IOException {
-        byte[] fileBytes = getFIleBytes(filename);
+    public long signFileMD5(String pathFile, long c, long n) throws IOException {
+        String checksumMD5 = DigestUtils.md5Hex(new FileInputStream(pathFile));
+        BigInteger hash = new BigInteger(checksumMD5, 16);
+        hash = hash.mod(new BigInteger(String.valueOf(n)));
 
-        long res = 0, p = 1;
+        long S = powMod(hash.longValue(), c, n);
+        return S;
+    }
 
-        for (int i = 0; i < fileBytes.length; i++, p *= 3733) {
-            res += fileBytes[i] * p % 2 ^ 63;
+    public void checkSign(String pathFile, long s, long d, long n) throws IOException, SignatureException {
+        String checksumMD5 = DigestUtils.md5Hex(new FileInputStream(pathFile));
+        BigInteger hash = new BigInteger(checksumMD5, 16);
+        hash = hash.mod(new BigInteger(String.valueOf(n)));
+
+        long w = powMod(s, d, n);
+
+        System.out.println("message = "+hash+" check = "+w);
+
+        if (w != hash.longValue()) {
+            throw new SignatureException("digital signature RSA is invalid");
         }
-        return res;
     }
 }
